ae39c4c9b863d14e3086114cbb8a9baa587f76b8
linhnv2nm <muadongyeuthuong3x@gmail.com>
2022-05-21 10:55:18 +0700

    haha

---
+++ App.js
@@ -0,0 +1,34 @@
+
+import * as React from 'react';
+import firebase from '@react-native-firebase/app';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import SignInScreen from "./screen/Login"
+import TabBottom from "./navigate/tabBottom"
+const Stack = createNativeStackNavigator();
+const firebaseConfig = {
+  apiKey: "AIzaSyBZkh8nkvgcv6HUWCftIJPkuSZANMvlVLU",
+  authDomain: "tathuheo.firebaseapp.com",
+  databaseURL: "https://tathuheo-default-rtdb.firebaseio.com",
+  projectId: "tathuheo",
+  storageBucket: "tathuheo.appspot.com",
+  messagingSenderId: "65211879809",
+  appId: "1:277423826521:android:baf9811bf6ad9c4cd9f348",
+  measurementId: "277423826521"
+};
+
+export default function App() {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerShown: false
+      }}>
+       <Stack.Screen name="SignIn" component={SignInScreen} />
+       <Stack.Screen name="ListUser" component={TabBottom} /> 
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
